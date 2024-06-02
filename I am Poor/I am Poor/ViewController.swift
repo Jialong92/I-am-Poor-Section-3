dba62d6a9755c9e4bc16c82cd978bdc44b109dcd
Leong Jia Long <84724553+Jialong92@users.noreply.github.com>
2024-06-03 06:57:18 +0800

    Add files via upload

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Jialong Leong on 3/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
